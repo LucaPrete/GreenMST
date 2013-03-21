e8db7aa5234b0b3b7e953e135f6026c8efe8e1fd
Andrea Biancini <andrea.biancini@gmail.com>
2013-03-21 14:51:30 +0000

    Floodlight version of GreenMST module

---
+++ src/it/garr/greenmst/web/GreenMSTWebRoutable.java
@@ -0,0 +1,36 @@
+package it.garr.greenmst.web;
+
+import net.floodlightcontroller.restserver.RestletRoutable;
+
+import org.restlet.Context;
+import org.restlet.Restlet;
+import org.restlet.routing.Router;
+
+/**
+ * Class to describe REST routes for the GreenMST service.
+ *
+ * @author Luca Prete <luca.prete@garr.it>
+ * @author Andrea Biancini <andrea.biancini@garr.it>
+ * @author Fabio Farina <fabio.farina@garr.it>
+ * 
+ * @version 0.8
+ * @see net.floodlightcontroller.restserver.RestletRoutable
+ * @see it.garr.greenmst.web.TopoEdgesResource
+ * @see it.garr.greenmst.web.RedundantEdgesResource
+ *
+ */
+
+public class GreenMSTWebRoutable implements RestletRoutable {
+    @Override
+    public Restlet getRestlet(Context context) {
+        Router router = new Router(context);
+        router.attach("/topoedges/json", TopoEdgesResource.class);
+        router.attach("/redundantedges/json", RedundantEdgesResource.class);
+        return router;
+    }
+
+    @Override
+    public String basePath() {
+        return "/wm/greenmst";
+    }
+}
