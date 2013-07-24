68694ed7fdf8eb31c74ee228389dee4fec026f63
LucaPrete <preteluca@gmail.com>
2013-07-24 12:00:45 +0200

    Copyright and licence added

--- scripts/startTopo.sh
+++ scripts/startTopo.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+########################################################
+#
+# Copyright (C) 2013 Luca Prete, Andrea Biancini, Fabio Farina - www.garr.it - Consortium GARR
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# 
+# author Luca Prete <luca.prete@garr.it>
+# author Andrea Biancini <andrea.biancini@garr.it>
+# author Fabio Farina <fabio.farina@garr.it>
+#
+########################################################
 
 sudo mn --controller=remote,ip=127.0.0.1 --custom ~/mininet/custom/four-switch.py --topo=foursw --mac
 
--- src/it/garr/greenmst/IGreenMSTService.java
+++ src/it/garr/greenmst/IGreenMSTService.java
@@ -1,5 +1,43 @@
-package it.garr.greenmst;
+/**
+ * Copyright (C) 2013 Luca Prete, Andrea Biancini, Fabio Farina - www.garr.it - Consortium GARR
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Public interface for the Floodlight GreenMST service.
+ * 
+ * @author Luca Prete <luca.prete@garr.it>
+ * @author Andrea Biancini <andrea.biancini@garr.it>
+ * @author Fabio Farina <fabio.farina@garr.it>
+ * 
+ * @version 0.90
+ * @see it.garr.greenmst.GreenMST
+ * @see it.garr.greenmst.TopologyCostsLoader
+ * @see it.garr.greenmst.algorithms.IMinimumSpanningTreeAlgorithm
+ * @see it.garr.greenmst.algorithms.KruskalAlgorithm
+ * @see it.garr.greenmst.types.LinkWithCost
+ * @see it.garr.greenmst.types.TopologyCosts
+ * @see it.garr.greenmst.web.GreenMSTWebRoutable
+ * @see it.garr.greenmst.web.MSTEdgesResource
+ * @see it.garr.greenmst.web.RedundantEdgesResource
+ * @see it.garr.greenmst.web.TopoCostsResource
+ * @see it.garr.greenmst.web.TopoEdgesResource
+ * @see it.garr.greenmst.web.serializers.LinkWithCostJSONSerializer
+ * @see it.garr.greenmst.web.serializers.TopologyCostsJSONDeserializer
+ * @see it.garr.greenmst.web.serializers.TopologyCostsJSONSerializer
+ * 
+ */
 
+package it.garr.greenmst;
 
 import it.garr.greenmst.types.LinkWithCost;
 import it.garr.greenmst.types.TopologyCosts;
@@ -7,18 +45,6 @@ import it.garr.greenmst.types.TopologyCosts;
 import java.util.Set;
 
 import net.floodlightcontroller.core.module.IFloodlightService;
-
-/**
- * Public interface for the Floodlight GreenMST service.
- *
- * @author Luca Prete <luca.prete@garr.it>
- * @author Andrea Biancini <andrea.biancini@garr.it>
- * @author Fabio Farina <fabio.farina@garr.it>
- * 
- * @version 0.8
- * @see net.floodlightcontroller.core.module.IFloodlightService
- *
- */
 
 public interface IGreenMSTService extends IFloodlightService {
 	
